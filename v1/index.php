200deb458606acfbf29f29d68b873cf32448658e
Yuval Marcus <37221446+yuvalMarcus@users.noreply.github.com>
2020-12-26 22:32:31 +0200

    Update index.php

--- v1/index.php
+++ v1/index.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-*/
+  ini_set('display_errors', '1');
+  ini_set('display_startup_errors', '1');
+  error_reporting(E_ALL);
+ */
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
@@ -15,11 +15,22 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 require_once './hepler/hepler.php';
 
 spl_autoload_register(function($file) {
-    
+
     if (is_file("./$file.php")) {
 
         require "./$file.php";
     }
 });
+
+try {
+    $dataBase = new \model\DataBase();
+} catch (PDOException $e) {
+    $response = new \controller\Response();
+    $response->setSuccess(false);
+    $response->setHttpStatusCode(500);
+    $response->addMessage("DataBase not found");
+    $response->send();
+    exit;
+}
 
 require_once './router/api.php';
